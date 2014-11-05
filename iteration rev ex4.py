2046da4d79b52063b87ae3fb15d1767a3dde101f
Nichollsean <40668@longroad.ac.uk>
2014-11-05 19:53:55 +0000

    Holiday work

---
+++ iteration rev ex4.py
@@ -0,0 +1,10 @@
+#Sean Nicholls
+#iteration dev ex4
+
+number = 0
+while number == 0:
+    number = int(input("Please enter a number between 10 and 20: "))
+    if number >=10 and number <=20:
+        print("Thank you")
+    else:
+        print("Try again")
